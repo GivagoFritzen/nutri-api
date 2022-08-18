5b2219600bbb25051760092a67a68a1e465daaf7
Givago <givago.fritzen@hotmail.com>
2022-08-17 22:00:05 -0300

    Ajuste service register

--- API/Setup/DependencyInjection.cs
+++ API/Setup/DependencyInjection.cs
@@ -45,7 +45,7 @@ namespace API.Setup
             services.AddScoped<IEventPublisher, RabbitMQPublisher>();
 
             //  Nutricionista
-            services.AddSingleton<IApplicationServiceNutricionista, ApplicationServiceNutricionista>();
+            services.AddScoped<IApplicationServiceNutricionista, ApplicationServiceNutricionista>();
             services.AddScoped<INutricionistaRepository, NutricionistaRepository>();
             services.AddTransient<IRepositorySQL<NutricionistaEntity>, RepositorySQLBase<NutricionistaEntity>>();
 
